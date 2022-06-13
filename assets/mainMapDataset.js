f487f957bbe52bf9dcd28d9e1645724d09f4e918
alviansyafril <alviansyafril@gmail.com>
2022-06-13 23:25:44 +0700

    cleaun up code

--- assets/mainMapDataset.js
+++ assets/mainMapDataset.js
@@ -2,123 +2,123 @@
 const mainMapDataset = [
   {
     jam: '10:00',
-    img: require('./images/santanu001.png'),
+    img: require('./images/mainMap/santanu001.png'),
   },
   {
     jam: '10:02',
-    img: require('./images/santanu002.png'),
+    img: require('./images/mainMap/santanu002.png'),
   },
   {
     jam: '10:04',
-    img: require('./images/santanu003.png'),
+    img: require('./images/mainMap/santanu003.png'),
   },
   {
     jam: '10:06',
-    img: require('./images/santanu004.png'),
+    img: require('./images/mainMap/santanu004.png'),
   },
   {
     jam: '10:08',
-    img: require('./images/santanu005.png'),
+    img: require('./images/mainMap/santanu005.png'),
   },
   {
     jam: '10:10',
-    img: require('./images/santanu006.png'),
+    img: require('./images/mainMap/santanu006.png'),
   },
   {
     jam: '10:12',
-    img: require('./images/santanu007.png'),
+    img: require('./images/mainMap/santanu007.png'),
   },
   {
     jam: '10:14',
-    img: require('./images/santanu008.png'),
+    img: require('./images/mainMap/santanu008.png'),
   },
   {
     jam: '10:16',
-    img: require('./images/santanu009.png'),
+    img: require('./images/mainMap/santanu009.png'),
   },
   {
     jam: '10:18',
-    img: require('./images/santanu0010.png'),
+    img: require('./images/mainMap/santanu0010.png'),
   },
   {
     jam: '10:20',
-    img: require('./images/santanu0011.png'),
+    img: require('./images/mainMap/santanu0011.png'),
   },
   {
     jam: '10:22',
-    img: require('./images/santanu0012.png'),
+    img: require('./images/mainMap/santanu0012.png'),
   },
   {
     jam: '10:24',
-    img: require('./images/santanu0013.png'),
+    img: require('./images/mainMap/santanu0013.png'),
   },
   {
     jam: '10:26',
-    img: require('./images/santanu0014.png'),
+    img: require('./images/mainMap/santanu0014.png'),
   },
   {
     jam: '10:28',
-    img: require('./images/santanu0015.png'),
+    img: require('./images/mainMap/santanu0015.png'),
   },
   {
     jam: '10:30',
-    img: require('./images/santanu0016.png'),
+    img: require('./images/mainMap/santanu0016.png'),
   },
   {
     jam: '10:32',
-    img: require('./images/santanu0017.png'),
+    img: require('./images/mainMap/santanu0017.png'),
   },
   {
     jam: '10:34',
-    img: require('./images/santanu0018.png'),
+    img: require('./images/mainMap/santanu0018.png'),
   },
   {
     jam: '10:36',
-    img: require('./images/santanu0019.png'),
+    img: require('./images/mainMap/santanu0019.png'),
   },
   {
     jam: '10:38',
-    img: require('./images/santanu0020.png'),
+    img: require('./images/mainMap/santanu0020.png'),
   },
   {
     jam: '10:40',
-    img: require('./images/santanu0021.png'),
+    img: require('./images/mainMap/santanu0021.png'),
   },
   {
     jam: '10:42',
-    img: require('./images/santanu0022.png'),
+    img: require('./images/mainMap/santanu0022.png'),
   },
   {
     jam: '10:44',
-    img: require('./images/santanu0023.png'),
+    img: require('./images/mainMap/santanu0023.png'),
   },
   {
     jam: '10:46',
-    img: require('./images/santanu0024.png'),
+    img: require('./images/mainMap/santanu0024.png'),
   },
   {
     jam: '10:48',
-    img: require('./images/santanu0025.png'),
+    img: require('./images/mainMap/santanu0025.png'),
   },
   {
     jam: '10:50',
-    img: require('./images/santanu0026.png'),
+    img: require('./images/mainMap/santanu0026.png'),
   },
   {
     jam: '10:52',
-    img: require('./images/santanu0027.png'),
+    img: require('./images/mainMap/santanu0027.png'),
   },
   {
     jam: '10:54',
-    img: require('./images/santanu0028.png'),
+    img: require('./images/mainMap/santanu0028.png'),
   },
   {
     jam: '10:56',
-    img: require('./images/santanu0029.png'),
+    img: require('./images/mainMap/santanu0029.png'),
   },
   {
     jam: '10:58',
-    img: require('./images/santanu0030.png'),
+    img: require('./images/mainMap/santanu0030.png'),
   },
 ];
 
--- assets/miniMapDataset.js
+++ assets/miniMapDataset.js
@@ -2,123 +2,123 @@
 const miniMapDataset = [
   {
     jam: '10:00',
-    img: require('./images/santanu001.png'),
+    img: require('./images/miniMap/santanu001.png'),
   },
   {
     jam: '10:02',
-    img: require('./images/santanu002.png'),
+    img: require('./images/miniMap/santanu002.png'),
   },
   {
     jam: '10:04',
-    img: require('./images/santanu003.png'),
+    img: require('./images/miniMap/santanu003.png'),
   },
   {
     jam: '10:06',
-    img: require('./images/santanu004.png'),
+    img: require('./images/miniMap/santanu004.png'),
   },
   {
     jam: '10:08',
-    img: require('./images/santanu005.png'),
+    img: require('./images/miniMap/santanu005.png'),
   },
   {
     jam: '10:10',
-    img: require('./images/santanu006.png'),
+    img: require('./images/miniMap/santanu006.png'),
   },
   {
     jam: '10:12',
-    img: require('./images/santanu007.png'),
+    img: require('./images/miniMap/santanu007.png'),
   },
   {
     jam: '10:14',
-    img: require('./images/santanu008.png'),
+    img: require('./images/miniMap/santanu008.png'),
   },
   {
     jam: '10:16',
-    img: require('./images/santanu009.png'),
+    img: require('./images/miniMap/santanu009.png'),
   },
   {
     jam: '10:18',
-    img: require('./images/santanu0010.png'),
+    img: require('./images/miniMap/santanu0010.png'),
   },
   {
     jam: '10:20',
-    img: require('./images/santanu0011.png'),
+    img: require('./images/miniMap/santanu0011.png'),
   },
   {
     jam: '10:22',
-    img: require('./images/santanu0012.png'),
+    img: require('./images/miniMap/santanu0012.png'),
   },
   {
     jam: '10:24',
-    img: require('./images/santanu0013.png'),
+    img: require('./images/miniMap/santanu0013.png'),
   },
   {
     jam: '10:26',
-    img: require('./images/santanu0014.png'),
+    img: require('./images/miniMap/santanu0014.png'),
   },
   {
     jam: '10:28',
-    img: require('./images/santanu0015.png'),
+    img: require('./images/miniMap/santanu0015.png'),
   },
   {
     jam: '10:30',
-    img: require('./images/santanu0016.png'),
+    img: require('./images/miniMap/santanu0016.png'),
   },
   {
     jam: '10:32',
-    img: require('./images/santanu0017.png'),
+    img: require('./images/miniMap/santanu0017.png'),
   },
   {
     jam: '10:34',
-    img: require('./images/santanu0018.png'),
+    img: require('./images/miniMap/santanu0018.png'),
   },
   {
     jam: '10:36',
-    img: require('./images/santanu0019.png'),
+    img: require('./images/miniMap/santanu0019.png'),
   },
   {
     jam: '10:38',
-    img: require('./images/santanu0020.png'),
+    img: require('./images/miniMap/santanu0020.png'),
   },
   {
     jam: '10:40',
-    img: require('./images/santanu0021.png'),
+    img: require('./images/miniMap/santanu0021.png'),
   },
   {
     jam: '10:42',
-    img: require('./images/santanu0022.png'),
+    img: require('./images/miniMap/santanu0022.png'),
   },
   {
     jam: '10:44',
-    img: require('./images/santanu0023.png'),
+    img: require('./images/miniMap/santanu0023.png'),
   },
   {
     jam: '10:46',
-    img: require('./images/santanu0024.png'),
+    img: require('./images/miniMap/santanu0024.png'),
   },
   {
     jam: '10:48',
-    img: require('./images/santanu0025.png'),
+    img: require('./images/miniMap/santanu0025.png'),
   },
   {
     jam: '10:50',
-    img: require('./images/santanu0026.png'),
+    img: require('./images/miniMap/santanu0026.png'),
   },
   {
     jam: '10:52',
-    img: require('./images/santanu0027.png'),
+    img: require('./images/miniMap/santanu0027.png'),
   },
   {
     jam: '10:54',
-    img: require('./images/santanu0028.png'),
+    img: require('./images/miniMap/santanu0028.png'),
   },
   {
     jam: '10:56',
-    img: require('./images/santanu0029.png'),
+    img: require('./images/miniMap/santanu0029.png'),
   },
   {
     jam: '10:58',
-    img: require('./images/santanu0030.png'),
+    img: require('./images/miniMap/santanu0030.png'),
   },
 ];
 
